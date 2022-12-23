b17379050d5ae7448f41578689e7304010e1e8b5
HoleHorse <tamirlankulzhanov@gmail.com>
2022-12-23 05:37:24 -0600

    tests

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -30,26 +30,26 @@ import com.example.affirmations.model.Affirmation
  * Adapter for the [RecyclerView] in [MainActivity]. Displays [Affirmation] data object.
  */
 class ItemAdapter(
-        private val context: Context,
-        private val dataset: List<Affirmation>
-): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    private val context: Context,
+    private val dataset: List<Affirmation>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just an Affirmation object.
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
         val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /**
-    * Create new views (invoked by the layout manager)
-    */
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
-                .inflate(R.layout.list_item, parent, false)
+            .inflate(R.layout.list_item, parent, false)
 
         return ItemViewHolder(adapterLayout)
     }
